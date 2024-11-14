ce82c9bde98ff35a55766f07e663a291799ab1ce
quttoshii <afaqalam2003@gmail.com>
2024-11-14 07:40:16 +0500

    Insights connected with DB

---
+++ htdocs/scholarwatch/includes/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost:3307';
+$db = 'scholarwatch';  // Database name
+$user = 'root';     // Database username
+$pass = 'root';     // Database password
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "successfully connected to db!\n";
+} catch (PDOException $e) {
+    die("Could not connect to the database $db :" . $e->getMessage());
+}
--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
 - To run the web app (react only):
-    cd /webapp
-    npm install 
-    npm run build
-    npm start
+    - cd /webapp
+    - npm install 
+    - npm run build
+    - npm start
+
+- To run the backend:
+    - install xampp
+    - replace xampp/htdocs with scholarwatch/htdocs
